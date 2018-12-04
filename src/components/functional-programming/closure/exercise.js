18fae8d9eefb0897e33e4078e6be9baf852b01d9
alexlbr <alex.lobera@gmail.com>
2018-12-04 22:45:22 +0000

    adds solution closure

--- src/components/functional-programming/closure/exercise.js
+++ src/components/functional-programming/closure/exercise.js
@@ -1,8 +1,12 @@
-function add() {}
+function add(x) {
+  return function(y) {
+    return x + y;
+  };
+}
 
 const addFive = add(5);
 
 let result;
-//result = addFive(7); // should output 12
+result = addFive(7); // should output 12
 
 console.log("addFive(7)", result);
