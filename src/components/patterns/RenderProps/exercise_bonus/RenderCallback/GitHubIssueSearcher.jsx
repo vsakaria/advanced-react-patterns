e9681b50ebd4f0708fd5ddfc922c1c15ac089613
Alex <alex@Alexs-MBP.home>
2019-02-21 08:55:23 +0000

    adds render props bonus solution

--- src/components/patterns/RenderProps/exercise_bonus/RenderCallback/GitHubIssueSearcher.jsx
+++ src/components/patterns/RenderProps/exercise_bonus/RenderCallback/GitHubIssueSearcher.jsx
@@ -1,13 +1,58 @@
-/*
+import React from 'react'
+import searchGitHubIssues from '../utils/searchGitHubIssues'
+import { GITHUB_ISSUES_API_URL } from '../utils/config'
 
-Hint: You must move the logic from the Higher-Order Component here.
-Where is the logic in withGitHubIssueSearch or in GitHubIssueSearchInfiniteScroller?
+class GitHubIssueSearcher extends React.Component {
+constructor() {
+    super()
+    this.state = {
+    isFetching: false,
+    issues: [],
+    links: {}
+    }
+}
 
-Steps:
-1. Copy the logic from the HoC into this file
-2. The Render Props is a component and the HoC is a function. Once you copy
-   the logic from the HoC here you will have to remove the main function
-3. Refactor the return of the render method of the HoC code in this file to
-   "execute" the children and pass the right arguments
+componentDidMount() {
+    const {
+    query = 'react',
+    label = 'bug',
+    language = 'javascript',
+    state = 'open',
+    sort = 'sort',
+    order = 'asc'
+    } = this.props
 
-*/
+    this.fetch(`${GITHUB_ISSUES_API_URL}?q=${query}+label:${label}+language:${language}+state:${state}&sort=${sort}&order=${order}`)
+}
+
+fetchNextPage = () => {
+    this.fetch(this.state.links.next)
+}
+
+fetch = (url) => {
+    this.setState({ isFetching: true })
+    searchGitHubIssues(url, (issues, links) => {
+    this.setState({
+        isFetching: false,
+        issues: [...this.state.issues, ...issues],
+        links
+    })
+    })
+}
+
+render() {
+    const fetchNextPage = !this.state.isFetching && this.state.links.next ?
+    this.fetchNextPage :
+    null
+    const { issues, isFetching } = this.state
+
+    return <MyComponent
+    {...this.props}
+    issues={issues}
+    fetchNextPage={fetchNextPage}
+    isFetching={isFetching}
+    />
+}
+}
+
+export default GitHubIssueSearcher
--- src/components/patterns/RenderProps/exercise_bonus/RenderCallback/index.jsx
+++ src/components/patterns/RenderProps/exercise_bonus/RenderCallback/index.jsx
@@ -1,8 +1,31 @@
-/*
+import React from 'react'
+import GitHubIssueSearcher from './GitHubIssueSearcher'
+import ScrollNotifier from '../ScrollNotifier'
 
-Hint:
-- Move the composed component into this file
-- GitHubIssueSearch is a component, not a function. You should import ./GitHubIssueSearcher
-  here and receive the arguments from GitHubIssueSearch using an arrow function
+const IssueInfiniteScroller = () => (
+    <GitHubIssueSearcher>
+        {({ issues, fetchNextPage, isFetching }) => (
+            <ScrollNotifier
+                style={{ height: 400 }}
+                onScrollAtTheBottom={ fetchNextPage }
+            >
+                <ul className="issue-list">
+                    {issues.map(item => (
+                        <li key={item.id}>
+                        <a href={item.html_url} target="_blank">
+                            <h3>{item.title}</h3>
+                            <p>{item.body}</p>
+                        </a>
+                        </li>
+                    ))}
+                    {!isFetching ? null : <li className="text-center" key="loading">
+                        <h2><i className="fa fa-cog fa-spin fa-3x fa-fw margin-bottom"></i></h2>
+                        </li>
+                    }
+                </ul>
+            </ScrollNotifier>  
+        )}
+  </GitHubIssueSearcher>
+)
 
-*/
+export default IssueInfiniteScroller
