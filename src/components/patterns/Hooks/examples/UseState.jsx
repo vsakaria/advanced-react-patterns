78030df97c0f040816458a3c412a4ba5563c375b
alexlbr <alex.lobera@gmail.com>
2019-03-06 23:57:39 +0000

    adds solution hooks

--- src/components/patterns/Hooks/examples/UseState.jsx
+++ src/components/patterns/Hooks/examples/UseState.jsx
@@ -6,23 +6,4 @@ const Input = () => {
   return <input value={value} onChange={e => setValue(e.target.value)} />;
 };
 
-// class Input extends React.Component {
-//   state = {
-//     value: ""
-//   };
-
-//   onChange = e => {
-//     this.setState({ value: e.target.value }, () => {
-//       console.log("Current Input state:", this.state.value);
-//     });
-//   };
-
-//   render() {
-//     const { value } = this.state;
-//     const { onChange } = this;
-
-//     return <input value={value} onChange={onChange} />;
-//   }
-// }
-
 export default Input;
