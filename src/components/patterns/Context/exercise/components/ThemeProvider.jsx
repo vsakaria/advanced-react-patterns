7660cce4c03cbab3a604a2736195f34e22f73e52
alexlbr <alex.lobera@gmail.com>
2019-03-08 15:01:53 +0000

    renames ThemeContext into ThemeProvider

---
+++ src/components/patterns/Context/exercise/components/ThemeProvider.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import themes from '../theme'
+import { ThemeProvider as StyledProvider } from 'styled-components'
+
+export const ThemeContext = React.createContext({});
+
+class ThemeProvider extends React.Component {
+  state = {
+    theme: "light"
+  }
+
+  handleThemeToggle = () => {
+    this.setState(prevState => ({theme: prevState.theme === 'light' ? 'dark' : 'light'}))
+  }
+
+  render() {
+    const { theme } = this.state;
+    const { children } = this.props;
+    return (
+      <ThemeContext.Provider value={{theme, setValue: this.handleThemeToggle}}>
+        <StyledProvider theme={themes[theme]}>
+          {children}
+        </StyledProvider>
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+export default ThemeProvider
