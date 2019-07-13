27020ffbab02b5688733284c5fb73f87f3027633
Vishal Sakaria <vsakaria@gmail.com>
2019-07-13 10:25:06 +0100

    UPDATE to currying

--- src/components/functional-programming/closure/exercise.js
+++ src/components/functional-programming/closure/exercise.js
@@ -1,11 +1,5 @@
-function add(x) {
-  return function(y) {
-    return x + y;
-  };
-}
+const add = x => y => x + y;
 
-// or
-// const add = x => y => x + y;
 const addFive = add(5);
 const result = addFive(5); // should output 12
 
