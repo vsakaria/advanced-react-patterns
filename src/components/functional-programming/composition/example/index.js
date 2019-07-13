cbd8a2f16b9e1bb53d70dd7b6c9503581d282837
Vishal Sakaria <vsakaria@gmail.com>
2019-07-13 10:57:52 +0100

    UPDATE composition of transformToAmazingLower

--- src/components/functional-programming/composition/example/index.js
+++ src/components/functional-programming/composition/example/index.js
@@ -17,12 +17,12 @@ export const alphaUpperCaseTransform = compose(
 /*
  Here we use default params to ensure only string are pass when undefined is passed through
  We may need to use typeof here
- Typescript work well in these use case hence functional programming languages are strongly typed.
+ Typescript return types and param types work well in these use case hence functional programming languages are strongly typed.
  Also TS optional chaining TC39 is being reviewed. https://medium.com/inside-rimeto/optional-chaining-in-typescript-622c3121f99b
 */
 
 const toLowerCase = (text = "") => text.toLowerCase();
-const findAmazing = (text = "") => text.includes("amazing");
+const findAmazing = (text = "") => (text.includes("amazing") ? "amazing" : "");
 
 export const transformToAmazingLower = compose(
   toLowerCase,
