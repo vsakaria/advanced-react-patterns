ff6dcd1eb4dc31879c7b2dec1dfcd9f69549c5ed
Vishal Sakaria <vsakaria@gmail.com>
2019-07-13 10:27:01 +0100

    UPDATE questiosn

--- src/components/functional-programming/closure/exercise.js
+++ src/components/functional-programming/closure/exercise.js
@@ -4,3 +4,13 @@ const addFive = add(5);
 const result = addFive(5); // should output 12
 
 console.log("Closure", result);
+
+/*
+1- Is the inner function pure?
+Yes because once it is returned the inner state x is no longer important.
+addFive for all intense and purposes is pure.
+
+2- Want is executed first, the inner of the outer function?
+Outer which returns the inner which is then exectuted.
+
+*/
